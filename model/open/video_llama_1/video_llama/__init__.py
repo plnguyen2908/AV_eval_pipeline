ea626c8a8374ec19dedbed513165ab03a618f34a
plnguyen2908 <plnguyen6@wisc.edu>
2025-06-27 00:53:14 +0900

    Remove nested git dirs so ckpt tracked as normal files

---
+++ model/open/video_llama_1/video_llama/__init__.py
@@ -0,0 +1,44 @@
+"""
+ Copyright (c) 2022, salesforce.com, inc.
+ All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+ For full license text, see the LICENSE_Lavis file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
+import os
+import sys
+
+from omegaconf import OmegaConf
+
+from video_llama.common.registry import registry
+
+from video_llama.datasets.builders import *
+from video_llama.models import *
+from video_llama.processors import *
+from video_llama.tasks import *
+
+
+root_dir = os.path.dirname(os.path.abspath(__file__))
+# print(root_dir)
+default_cfg = OmegaConf.load(os.path.join(root_dir, "configs/default.yaml"))
+
+# def safe_register_path(name, path):
+#     try:
+#         registry.register_path(name, path)
+#     except KeyError:
+#         # đã có => bỏ qua hoặc in log
+#         pass
+# safe_register_path("library_root", root_dir)
+# repo_root = os.path.join(root_dir, "..")
+# safe_register_path("repo_root", repo_root)
+# cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
+# safe_register_path("cache_root", cache_root)
+
+registry.register_path("library_root", root_dir)
+repo_root = os.path.join(root_dir, "..")
+registry.register_path("repo_root", repo_root)
+cache_root = os.path.join(repo_root, default_cfg.env.cache_root)
+registry.register_path("cache_root", cache_root)
+
+registry.register("MAX_INT", sys.maxsize)
+registry.register("SPLIT_NAMES", ["train", "val", "test"])
