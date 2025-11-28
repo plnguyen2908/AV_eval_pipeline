75843b569933fa983856a8f675844d52d8d0f171
plnguyen2908 <plnguyen6@wisc.edu>
2025-11-29 07:50:26 +0900

    fix main logic

--- download_data.py
+++ download_data.py
@@ -1,5 +1,5 @@
 from huggingface_hub import snapshot_download
 # from .get_dataset import get_dataset
 
-local_dir = "/data/plnguyen2908/AV_eval_pipeline/data"
+local_dir = "/data/plnguyen2908/AV-SpeakerBench/data"
 snapshot_download("plnguyen2908/Holistic_AVQA_bench", repo_type="dataset", local_dir=local_dir, local_dir_use_symlinks=False)
--- model/closed/gemini/inference.py
+++ model/closed/gemini/inference.py
@@ -1,26 +1,48 @@
 from google import genai
 from google.genai import types
-import os
+import os, time
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv('GEMINI_API_KEY')
-client = genai.Client(api_key=api_key)
+def gemini_process(media_path, question, model_name, idx):
+    # choose API key shard
+    # api_key = os.getenv(f'GEMINI_API_KEY{idx % 8}')
+    api_key = os.getenv(f'KEY')
+    client = genai.Client(api_key=api_key)
 
-def gemini_2p0_flash_process(video_path, question):
-    with open(video_path, 'rb') as f:
-        video_bytes = f.read()
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=types.Content(
-            parts=[
-                types.Part(
-                    inline_data=types.Blob(data=video_bytes, mime_type='video/mp4')
-                ),
-                types.Part(text=question)
-            ]
-        )
-    )
+    # decide mime type by file extension
+    ext = os.path.splitext(media_path)[1].lower()
+    mime = 'audio/wav' if ext == '.wav' else 'video/mp4'
 
-    return response.text
+    print(media_path, mime)
+
+    with open(media_path, 'rb') as f:
+        payload = f.read()
+
+    while True:
+        try:
+            if "pro" in model_name or "2.5" not in model_name:
+                resp = client.models.generate_content(
+                    model=model_name,
+                    contents=types.Content(parts=[
+                        types.Part(inline_data=types.Blob(data=payload, mime_type=mime)),
+                        types.Part(text=question),
+                    ])
+                )
+            else:
+                print(f"{model_name} not thinking")
+                resp = client.models.generate_content(
+                    model=model_name,
+                    contents=types.Content(parts=[
+                        types.Part(inline_data=types.Blob(data=payload, mime_type=mime)),
+                        types.Part(text=question),
+                    ]),
+                    config = types.GenerateContentConfig(
+                        thinking_config = types.ThinkingConfig(thinking_budget = 0)
+                    )
+                )
+            return resp.text
+        except Exception as e:
+            print(f"Got exception: {e}, retry again.")
+            time.sleep(40)
